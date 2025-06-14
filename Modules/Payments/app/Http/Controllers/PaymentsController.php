a2f7a3b96932b6ca5929542db3c3796b64f59709
ammar <ammar@email.com>
2025-06-15 00:21:03 +0300

    view Payments: Retrieve payment details for a specific order or all payments.

--- Modules/Payments/app/Http/Controllers/PaymentsController.php
+++ Modules/Payments/app/Http/Controllers/PaymentsController.php
@@ -2,55 +2,25 @@
 
 namespace Modules\Payments\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\Payments\Models\Payment;
 
 class PaymentsController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('payments::index');
+        $query = Payment::with('order')->whereHas('order', function ($q) {
+            $q->where('user_id', auth('api')->id());
+        });
+
+        if ($request->has('order_id')) {
+            $query->where('order_id', $request->order_id);
+        }
+
+        return lynx()->message('get successfully')->data([$query->paginate(10)])->response();
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('payments::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {}
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
-    {
-        return view('payments::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('payments::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
 }
--- Modules/Payments/routes/api.php
+++ Modules/Payments/routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Payments\Http\Controllers\PaymentsController;
 use Modules\Payments\Http\Controllers\PaymentGatewaysController;
 
 
 
-Route::prefix('payment')->group(function () {
+Route::prefix('payment')->middleware('auth:api')->group(function () {
     Route::get('/pay/{gateway}', [PaymentGatewaysController::class, 'pay']);
     Route::get('/callback/{gateway}', [PaymentGatewaysController::class, 'handleCallback']);
+    Route::get('/all', [PaymentsController::class, 'index']);
 });
